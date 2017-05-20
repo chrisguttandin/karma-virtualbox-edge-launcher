c94986eac3fde9c27c12a572cdeb89fd32ee4379
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-20 17:06:10 +0200

    add a simple test

---
+++ test/unit/module.js
@@ -0,0 +1,9 @@
+const virtualBoxEdgeLauncher = require('../../src/module.js');
+
+describe('karma-virtualbox-edge-launcher', () => {
+
+    it('should export the launcher', () => {
+        expect(virtualBoxEdgeLauncher['launcher:VirtualBoxEdge']).to.defined;
+    });
+
+});
