b24bc8f6fb7581185dca118752f162d8e9323061
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-28 22:55:28 +0200

    fix test

--- test/unit/module.js
+++ test/unit/module.js
@@ -3,7 +3,7 @@ const virtualBoxEdgeLauncher = require('../../src/module.js');
 describe('karma-virtualbox-edge-launcher', () => {
 
     it('should export the launcher', () => {
-        expect(virtualBoxEdgeLauncher['launcher:VirtualBoxEdge']).to.defined;
+        expect(virtualBoxEdgeLauncher['launcher:VirtualBoxEdge']).to.not.be.undefined;
     });
 
 });
